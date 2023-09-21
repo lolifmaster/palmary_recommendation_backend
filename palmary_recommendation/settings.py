882a10f5194e0ecb8e792241538930d7b0ab5adb
Mounir <polomnr1@gmail.com>
2023-09-21 12:32:36 +0100

    add custom user model

--- palmary_recommendation/settings.py
+++ palmary_recommendation/settings.py
@@ -124,4 +124,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'user.User'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- user/models.py
+++ user/models.py
@@ -4,13 +4,43 @@ from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
 
 
+class UserManager(BaseUserManager):
 
-class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=True, validators=[RegexValidator(r'^[\w.@+-]+$')])
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(username, email, password, **extra_fields)
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError(_('The username must be set'))
+        if not email:
+            raise ValueError(_('The email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(username=username.capitalize(), email=email, **extra_fields)
+        user.set_password(password)
+
+        user.save()
+        return user
+
+
+class User(AbstractUser, PermissionsMixin):
+    username = models.CharField(max_length=150, validators=[RegexValidator(r'[a-zA-Z][\w.-]{0,24}$',
+                                                                           _('username must be start with character following by (characters, digits, /_/-) max length is 25.'))])
     email = models.EmailField(_('email address'), unique=True)
     phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
-    REQUIRED_FIELDS = ['email', 'phone_number']
-    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
 
     def __str__(self):
         return self.username
