7ff415ee413cf0c2fd3a7276414480d4474d65bd
Mounir <polomnr1@gmail.com>
2023-09-21 12:43:43 +0100

    add product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(_('Name'), max_length=255, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(_('Price'), max_digits=10, decimal_places=2, default=0.00)
+    description = models.TextField(_('Description'), blank=True, null=True)
+    is_active = models.BooleanField(_('Is active'), default=True)
+    created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('Updated at'), auto_now=True)
+
+    objects = ProductManager()
+
+    def __str__(self):
+        return self.name + ' ' + str(self.category)
