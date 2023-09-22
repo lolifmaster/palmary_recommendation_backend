ef28c09adea6793a18f7f040ceb1eeb8abcbd3ad
Mounir <polomnr1@gmail.com>
2023-09-22 19:54:30 +0100

    make more precise exception

--- products/views.py
+++ products/views.py
@@ -1,16 +1,14 @@
-import os
-
+from django.utils.translation import gettext_lazy as _
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.filters import SearchFilter
-from rest_framework.decorators import action
-from rest_framework import status
-from django_filters.rest_framework import DjangoFilterBackend
-from django.utils.translation import gettext_lazy as _
 
 from products.models import Product, Category, Type
-from products.serializer import ProductSerializer, CategorySerializer, TypeSerializer
 from products.pagination import CustomCursorPagination
+from products.serializer import ProductSerializer, CategorySerializer, TypeSerializer
 
 
 class ProductView(ReadOnlyModelViewSet):
@@ -22,7 +20,8 @@ class ProductView(ReadOnlyModelViewSet):
     filterset_fields = ['category__name', 'type__name']
     search_fields = ['name', 'category__name', 'type__name']
 
-    @action(detail=False, methods=['post'], description=_('Get recommendation based on 3 selected products'), url_path='recommendation')
+    @action(detail=False, methods=['post'],
+            description=_('Get recommendation based on 3 selected products'), url_path='recommendation')
     def get_recommendation(self, request):
         selected_products = request.data.get('products')
         if len(selected_products) != 3:
@@ -30,18 +29,21 @@ class ProductView(ReadOnlyModelViewSet):
         try:
             products = Product.objects.filter(id__in=selected_products)
             if len(products) != 3:
-                return Response({'message': _('Please select 3 products')}, status=status.HTTP_400_BAD_REQUEST)
-            pwd = os.path.dirname(__file__)
+                return Response({'message': _('Please select 3 products')},
+                                status=status.HTTP_400_BAD_REQUEST)
             # save these products into a row in csv file
             with open('static/products.csv', 'a+') as file:
                 file.write(f'{products[0].id},{products[1].id},{products[2].id}\n')
             category = products[0].category
-            type = products[0].type
-            recommendation = Product.objects.filter(category=category, type=type).exclude(id__in=selected_products)
+            product_type = products[0].type
+            recommendation = Product.objects.filter(category=category,
+                                                    type=product_type).exclude(id__in=selected_products)
             serializer = self.get_serializer(recommendation, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({'message': _('Please select 3 products from out catalog')}, status=status.HTTP_400_BAD_REQUEST)
+        except AttributeError as e:
+            print(e)
+            return Response({'message': _('Please select 3 products from out catalog')},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class CategoryView(ReadOnlyModelViewSet):
