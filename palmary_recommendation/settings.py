e8eeceb7c77c1f56527149038f1656feb0665ac5
Mounir <polomnr1@gmail.com>
2023-10-04 22:28:49 +0100

    add allowed origins and fix products.csv

--- palmary_recommendation/settings.py
+++ palmary_recommendation/settings.py
@@ -146,4 +146,9 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173'
+]
